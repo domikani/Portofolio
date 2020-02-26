038bb7cea2427cdf05f1a324b996081586dbc857
domna <domnakanari@gmail.com>
2020-02-26 19:30:26 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+
+
+  {path: 'skills', loadChildren: () => import('./pages/skills/skills.module').then(m => m.SkillsModule)},
+
+  {path: 'projects', loadChildren: () => import('./pages/projects/projects.module').then(m => m.ProjectsModule)},
+
+  {path: 'blog', loadChildren: () => import('./pages/blog/blog.module').then(m => m.BlogModule)},
+
+  {path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule)},
+  {path: 'cv', loadChildren: () => import('./pages/cv/cv.module').then(m => m.CvModule) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
